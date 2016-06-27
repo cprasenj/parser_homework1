963f6020849dd16dfb16eefb5c5ae9ab5fa0b0e3
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2016-06-27 15:54:49 +0530

    Moved message into decorater and refactoring<Prasen/Ananthu>

--- compiler.js
+++ compiler.js
@@ -1,20 +1,33 @@
 var parser = require('./parser.js').parser;
+var stringUtil = require('util');
 var _ = require('lodash');
 var compiler = {};
 
+var decorator = {
+  'default' : (accumulations) => {
+    return accumulations.map((anAccumulation) => {
+      var object = anAccumulation['OBJECTS'];
+      return object.length > 1 ?
+      stringUtil.format('%s %s %s and %s',
+        anAccumulation['NAME'], anAccumulation['VERB'], _.dropRight(object).join(', '), _.last(object)) :
+      stringUtil.format('%s %s %s',
+        anAccumulation['NAME'], anAccumulation['VERB'], _.head(object));
+    })
+  }
+}
+
 compiler.group = function(parseTree) {
-  return parseTree.map(function(node) {
-    var mergedNodes = node['child'].reduce(function(result, child) {
-      return _.mergeWith(result, child, function(val1, val2) {
-        return _.flatten([val1, val2]);
-      });
-    });
+  return parseTree.map((node) => {
+    var mergedNodes = node['child'].reduce(
+      (result, child) =>  _.mergeWith(result, child,
+        (val1, val2) =>  _.flatten([val1, val2])));
     return _.zipObject(mergedNodes['header'], mergedNodes['child']);
   });
 }
 
 compiler.compile = function(inputFileName, strategies){
   return _.flowRight(
+    decorator['default'],
     _.valuesIn(strategies),
      compiler.group,
      parser.createParseTree
--- strategies.js
+++ strategies.js
@@ -1,28 +1,22 @@
 var _ = require('lodash');
-var stringUtil = require('util');
 
 var partitionNodes = function(parseTree, field) {
   return _.partition(parseTree, field);
 }
 
 exports.strategies = {
-  'multipleObjectSameVerb' : function(groupedTree) {
+  'multipleObjectSameVerb' : (groupedTree) => {
     var allVerbs = _.flowRight(_.uniq, _.values, _.mapValues)(groupedTree, 'VERB');
-    var partitionByVerb = allVerbs.reduce(function(result, aVerb) {
+    return allVerbs.reduce((result, aVerb) => {
       result.push(partitionNodes(groupedTree, {'VERB':aVerb})[0]);
       return result;
-    }, []);
-    return partitionByVerb.map(function(aPartition) {
+    }, []).map((aPartition) => {
       var head = _.head(aPartition);
-      var objects = aPartition.reduce(function(bucket, aNode) {
-        return _.flatten([bucket, aNode['OBJECT']]);
-      }, []);
-      return stringUtil.format('%s %s %s and %s',
-        head['NAME'], head['VERB'], _.dropRight(objects).join(', '), _.last(objects));
+      return {
+        'NAME': head['NAME'],
+        'VERB': head['VERB'],
+        'OBJECTS': aPartition.reduce((bucket, aNode) => _.flatten([bucket, aNode['OBJECT']]), [])
+      };
     })
   }
-
-  // '' : function() {
-  //
-  // }
 }
