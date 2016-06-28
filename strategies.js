877e770de6b8b56211bc371c1b3aba47d47b58a2
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2016-06-28 16:55:04 +0530

    removing extra method<Prasenjit>

--- strategies.js
+++ strategies.js
@@ -1,14 +1,10 @@
 var _ = require('lodash');
 
-var partitionNodes = function(parseTree, field) {
-  return _.partition(parseTree, field);
-}
-
 exports.strategies = {
   'multiplePersonMultipleObjectSameVerb' : (groupedTree) => {
     return _.flowRight(_.uniq, _.values, _.mapValues)(groupedTree, 'NAME')
     .reduce((result, aName) => {
-      result.push(partitionNodes(groupedTree, {'NAME':aName})[0]);
+      result.push(_.partition(groupedTree, {'NAME':aName})[0]);
       return result;
     }, [])
     .map((aPartition) => {
